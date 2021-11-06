9e4de4d4ee8af344b2a377ae9cbd99a3f2cb8d0f
Sonu Kumar Kushwaha <flyingsonu736@gmail.com>
2021-11-06 23:37:42 +0530

    Update contact.js

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -51,12 +51,11 @@ const Contact = () => {
       <h2 className="title">Get In Touch</h2>
 
       <p>
-        My inbox is always open.
-        Whether you have a question or just want to say hi, I'll try my best to get back to you!
+         My inbox is always open. Whether you have a question or just want to say hello, I'll try my best to get back to you! Feel free to mail me about any relevant job updates.
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
-        Send Email
+        Mail Me
       </a>
     </StyledContactSection>
   );
