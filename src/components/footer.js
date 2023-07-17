afa30c43066bda18820a4815d96249a803bf4625
Sonu Kumar Kushwaha <flyingsonu736@gmail.com>
2023-07-17 09:21:54 +0530

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -50,21 +50,7 @@ const StyledCredit = styled.div`
     padding: 10px;
   }
 
-  .github-stats {
-    margin-top: 10px;
-
-    & > span {
-      display: inline-flex;
-      align-items: center;
-      margin: 0 7px;
-    }
-    svg {
-      display: inline-block;
-      width: auto;
-      height: 15px;
-      margin-right: 5px;
-    }
-  }
+  
 `;
 
 const Footer = () => {
@@ -89,8 +75,6 @@ const Footer = () => {
   );
 };
 
-Footer.propTypes = {
-  githubInfo: PropTypes.object,
-};
+
 
 export default Footer;
