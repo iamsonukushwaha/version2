3e7be13e6ecfbafa75f0cbf48c5348ee84bd54cb
Sonu Kumar Kushwaha <flyingsonu736@gmail.com>
2023-07-17 08:15:56 +0530

    Update about.js

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -132,7 +132,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'HTML & (S)CSS', 'React', 'Node.js', 'SQL', 'C++', 'Java'];
+  const skills = ['JavaScript (ES6+)', 'HTML & (S)CSS', 'React', 'Node.js', 'SQL', 'C++', 'Java', 'Python'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
