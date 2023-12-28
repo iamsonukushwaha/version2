660b7792d49fe7cf671ed968954618b4ffd21b40
Sonu Kumar Kushwaha <flyingsonu736@gmail.com>
2023-12-28 04:24:34 +0000

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-8aa2567ae0ed02ac02f3.js"
+    "url": "webpack-runtime-179c1f4ee7f4b0313f81.js"
   },
   {
     "url": "framework-22d51f0f5ee9f1c9c6e1.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "44fbbb1dffaba32020d27444b16f4fdc"
+    "revision": "f4ec0432886b55ed847fc4690018c727"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-49be599b1ad8fa2a8465.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "adfdfa6a0175b33326c58b37b1c45f02"
+    "revision": "1e9b08e0a71e774419b8147d7dad4c17"
   },
   {
     "url": "polyfill-1d1b2ce50066059b8c94.js"
